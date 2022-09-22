cbca7a287a7d3c468c40664c474a9f3d113771f5
Eduardo Domingo <domined@MFCGD.COM>
2022-09-22 13:05:19 +0800

    Load Json Data

--- src/index.js
+++ src/index.js
@@ -4,34 +4,39 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+const fact = require('../src/Data/fact.json');
+const fake = require('../src/Data/fake.json');
+
+ 
+
 app.get("/",(req,res)=>{
-    return res.json(
-        [{
-            fake :"Test",
-            fact:"Test"
-        } 
-    ]);
+    let data=[];
+    data.push(fact);
+    data.push(fake);
+    return res.json(data);
    
 });
 
 app.get("/infos",(req,res)=>{
-    console.log(req.type);
-
-    return res.json(
-        [{
-             id: 1,
-             type: "type test",
-             text: "text test",
-             result: "test resut",
-             result2: req.type
-
-        } 
-    ]);
+    let data=[];
+        if(req.query.type==1){
+            data.push(fact);
+        }
+        else if (req.query.type==0){
+            data.push(fake);
+        }
+        else{
+            data.push(fact);
+            data.push(fact);
+        }
+    
+    
+    return res.json(data);
 });
 
 
 app.post("/infos",(req,res)=>{
-    console.log(req.type);
+    console.log(req);
     return res.json(
         [{
              id: 1,
