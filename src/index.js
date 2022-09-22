91728fa61624f1f759c66a6154f4cd00578055e7
mitdcjerson <jerson_lao@manulife.ca>
2022-09-22 16:03:19 +0800

    tensorflow

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import express, { request, response } from "express";
 import cors from "cors"
+import { suggestIcon, trainModel } from "./suggestions/model";
+import * as use from "@tensorflow-models/universal-sentence-encoder"
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const fact = require('../src/Data/fact.json');
 const fake = require('../src/Data/fake.json');
-
-
+let trainedModel = {};
+let sentenceEncoder = {}
 
 app.get("/", (req, res) => {
     let data = [];
     data.push(fact);
     data.push(fake);
+  
+    loadModel();
     return res.json(data);
 
 });
+
+const loadModel = async () => {
+    console.log("loadmodel");
+    sentenceEncoder = await use.load();
+
+    trainedModel = await trainModel(sentenceEncoder);
+    console.log("loadmodel2");
+    sentenceEncoder;
+
+  };
 
 app.get("/infos", (req, res) => {
     let data = [];
@@ -43,14 +57,14 @@ app.post("/infos", (req, res) => {
         });
     }
 
-    if (!req.body.result) {
+    if (!req.body.type) {
         return res.status(400).json({
-            "error": "result not found"
+            "error": "type not found"
         });
     }
     const info = {
-        name: req.body.name,
-        result: req.body.result
+        text: req.body.text,
+        result: req.body.type
     };
 
     if(req.body.type === 0)
@@ -64,16 +78,19 @@ app.post("/infos", (req, res) => {
     return res.json(info);
 });
 
-app.get("/check/:text", (req, res) => {
+app.post("/check", async (req, res) => {
 
-    const threshold = 0.50;
-    const predictedIcon = suggestIcon(model, encoder, taskName, threshold);
-
+    const threshold = 0.30;
+    console.log(req.body.text);
+    const predictedIcon = await suggestIcon(trainedModel, sentenceEncoder, req.body.text, threshold);
+ 
+    Promise.all([predictedIcon]);
+    console.log(predictedIcon);
     if (predictedIcon === undefined) {
         res.status(404).send();
     }
 
-    return predictedIcon;
+    return res.json(predictedIcon);
 });
 
 
--- src/suggestions/model.js
+++ src/suggestions/model.js
@@ -1,12 +1,14 @@
 import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
-const learnTodos = require("./data/learn_todos.json");
-const exerciseTodos = require("./data/exercise_todos.json");
+const learnTodos = require('../Data/fact.json');
+const exerciseTodos = require('../Data/fake.json');
 
 const trainTasks = learnTodos.concat(exerciseTodos);
 
 const MODEL_NAME = "suggestion-model";
 const N_CLASSES = 2;
+const FACT = "FACT";
+const FAKE = "FAKE";
 
 const encodeData = async (encoder, tasks) => {
   const sentences = tasks.map(t => t.text.toLowerCase());
@@ -15,20 +17,20 @@ const encodeData = async (encoder, tasks) => {
 };
 
 const trainModel = async encoder => {
-  try {
-    const loadedModel = await tf.loadLayersModel(
-      `localstorage://${MODEL_NAME}`
-    );
-    console.log("Using existing model");
-    return loadedModel;
-  } catch (e) {
-    console.log("Training new model");
-  }
+  // try {
+  //   const loadedModel = await tf.loadLayersModel(
+  //     `localstorage://${MODEL_NAME}`
+  //   );
+  //   console.log("Using existing model");
+  //   return loadedModel;
+  // } catch (e) {
+  //   console.log("Training new model");
+  // }
 
   const xTrain = await encodeData(encoder, trainTasks);
 
   const yTrain = tf.tensor2d(
-    trainTasks.map(t => [t.icon === "BOOK" ? 1 : 0, t.icon === "RUN" ? 1 : 0])
+    trainTasks.map(t => [t.result === FACT ? 1 : 0, t.result === FAKE ? 1 : 0])
   );
 
   const model = tf.sequential();
@@ -47,23 +49,23 @@ const trainModel = async encoder => {
     metrics: ["accuracy"]
   });
 
-  const lossContainer = document.getElementById("loss-cont");
+ // const lossContainer = document.getElementById("loss-cont");
 
-  await model.fit(xTrain, yTrain, {
-    batchSize: 32,
-    validationSplit: 0.1,
-    shuffle: true,
-    epochs: 150,
-    callbacks: tfvis.show.fitCallbacks(
-      lossContainer,
-      ["loss", "val_loss", "acc", "val_acc"],
-      {
-        callbacks: ["onEpochEnd"]
-      }
-    )
-  });
+  // await model.fit(xTrain, yTrain, {
+  //   batchSize: 32,
+  //   validationSplit: 0.1,
+  //   shuffle: true,
+  //   epochs: 150,
+  //   callbacks: tfvis.show.fitCallbacks(
+  //     null,
+  //     ["loss", "val_loss", "acc", "val_acc"],
+  //     {
+  //       callbacks: ["onEpochEnd"]
+  //     }
+  //   )
+  // });
 
-  await model.save(`localstorage://${MODEL_NAME}`);
+  //await model.save(`localstorage://${MODEL_NAME}`);
 
   return model;
 };
@@ -77,10 +79,12 @@ const suggestIcon = async (model, encoder, taskName, threshold) => {
 
   const prediction = await model.predict(xPredict).data();
 
+  console.log(prediction[0] +"" + prediction[1]);
+
   if (prediction[0] > threshold) {
-    return "BOOK";
+    return FACT;
   } else if (prediction[1] > threshold) {
-    return "RUN";
+    return FAKE;
   } else {
     return null;
   }
